1011472fa387d7ed3745a5bb42ed2fa4929bf911
christophbuehler <christoph@cbuehler.li>
2024-04-12 00:12:53 +0200

    Update user profile component template

--- apps/frontend/src/app/profile/app-user-profile.component.ts
+++ apps/frontend/src/app/profile/app-user-profile.component.ts
@@ -3,46 +3,40 @@ import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { map } from 'rxjs';
 import { CompanyComponent } from './company.component';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [CommonModule, CompanyComponent],
-  template: ` <h2>Company</h2>
-    <app-company></app-company>
-
-    <h2>Profile</h2>
-
-    <div class="content-layout">
-      <h1 id="page-title" class="content__title">Profile Page</h1>
-      <div class="content__body">
-        <p id="page-description">
-          <span>
-            You can use the <strong>ID Token</strong> to get the profile
-            information of an authenticated user.
-          </span>
-          <span>
-            <strong>Only authenticated users can access this page.</strong>
-          </span>
-        </p>
-
-        <ng-container *ngIf="user$ | async as user">
-          <div class="profile-grid">
-            <div class="profile__header">
-              <img [src]="user.picture" alt="Profile" class="profile__avatar" />
-              <div class="profile__headline">
-                <h2 class="profile__title">{{ user.name }}</h2>
-                <span class="profile__description">{{ user.email }}</span>
-              </div>
-            </div>
-            {{ code$ | async | json }}
-          </div>
-        </ng-container>
-      </div>
-    </div>`,
   standalone: true,
+  imports: [CommonModule, CompanyComponent],
+  template: `
+    <div class="flex gap-4 items-center mb-10" *ngIf="user() as user">
+      <img
+        class="shadow-md w-28 h-28 rounded-full"
+        [src]="user.picture"
+        alt="Profile"
+      />
+      <div class="flex flex-col">
+        <span><strong>Name:</strong> {{ user.name }}</span>
+        <span><strong>Nickname:</strong> {{ user.nickname }}</span>
+        <span><strong>Email:</strong> {{ user.email }}</span>
+        <p class="text-gray-700 mb-0">
+          If you need to update your information, please
+          <a
+            class="underline underline-offset-2"
+            href="mailto: contact@s1seven.com"
+            >contact S1SEVEN</a
+          >.
+        </p>
+      </div>
+    </div>
+
+    <h2>Company</h2>
+    <app-company></app-company>
+  `,
 })
 export class UserProfileComponent {
-  title = 'Decoded ID Token';
+  readonly user = toSignal(this.auth.user$);
 
   user$ = this.auth.user$;
   code$ = this.user$.pipe(
