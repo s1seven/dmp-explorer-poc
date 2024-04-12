8a0e290b8f663cc4181129a68b2242c163f05e10
christophbuehler <christoph@cbuehler.li>
2024-04-12 11:22:20 +0200

    Refactor BatchService to use HttpClient and add new methods for batch operations

--- apps/frontend/src/app/batches/batch.service.ts
+++ apps/frontend/src/app/batches/batch.service.ts
@@ -1,14 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { BatchDto } from '../shared/models';
+import { HttpClient } from '@angular/common/http';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class BatchService {
-  constructor() {}
-}
+  readonly batches = signal<BatchDto[]>([]);
+  private readonly httpClient = inject(HttpClient);
 
-// BATCHES
-// - getBatches
-// - createBatch
-// - assignBatch
-// - acceptBatch
-// - declineBatch
-// - reclaimBatch
+  createBatch(batch: BatchDto): Promise<BatchDto> {
+    return firstValueFrom(
+      this.httpClient.post<BatchDto>('/api/batches', batch)
+    );
+  }
+
+  async getBatches(): Promise<BatchDto[]> {
+    const batches = await firstValueFrom(
+      this.httpClient.get<BatchDto[]>('/api/batches')
+    );
+    this.batches.set(batches);
+    return batches;
+  }
+
+  sendBatch(batch: BatchDto, vatId: string): Promise<BatchDto> {
+    return firstValueFrom(
+      this.httpClient.post<BatchDto>('/api/batches/send', { batch, vatId })
+    );
+  }
+
+  acceptBatch(batch: BatchDto): Promise<BatchDto> {
+    return firstValueFrom(
+      this.httpClient.post<BatchDto>('/api/batches/accept', batch)
+    );
+  }
+
+  declineBatch(batch: BatchDto): Promise<BatchDto> {
+    return firstValueFrom(
+      this.httpClient.post<BatchDto>('/api/batches/decline', batch)
+    );
+  }
+
+  reclaimBatch(batch: BatchDto): Promise<BatchDto> {
+    return firstValueFrom(
+      this.httpClient.post<BatchDto>('/api/batches/reclaim', batch)
+    );
+  }
+}
