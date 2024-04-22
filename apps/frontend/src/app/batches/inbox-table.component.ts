471603637465aef26b34c4c79ace8581d940559a
Eamon <eamon0989@gmail.com>
2024-04-22 13:43:52 +0200

    refactor: improve styling on inbox table

--- apps/frontend/src/app/batches/inbox-table.component.ts
+++ apps/frontend/src/app/batches/inbox-table.component.ts
@@ -16,81 +16,69 @@ import { MatButtonModule } from '@angular/material/button';
   standalone: true,
   imports: [CommonModule, MatTableModule, RouterLink, MatButtonModule],
   template: `
-    <table mat-table [dataSource]="batches()">
+    <mat-table class="overflow-auto" [dataSource]="batches()">
       <!-- Lot Number Column -->
       <ng-container matColumnDef="lotNumber">
-        <th mat-header-cell *matHeaderCellDef>Lot Number</th>
-        <td mat-cell *matCellDef="let batch">{{ batch.lotNumber }}</td>
+        <mat-header-cell *matHeaderCellDef>Lot Number</mat-header-cell>
+        <mat-cell *matCellDef="let batch">{{ batch.lotNumber }}</mat-cell>
       </ng-container>
 
       <!-- Lead Content Column -->
       <ng-container matColumnDef="leadContent">
-        <th mat-header-cell *matHeaderCellDef>Lead Content</th>
-        <td mat-cell *matCellDef="let batch">{{ batch.leadContent }}</td>
+        <mat-header-cell *matHeaderCellDef>Lead Content</mat-header-cell>
+        <mat-cell *matCellDef="let batch">{{ batch.leadContent }}</mat-cell>
       </ng-container>
 
       <!-- Mercury Content Column -->
       <ng-container matColumnDef="mercuryContent">
-        <th mat-header-cell *matHeaderCellDef>Mercury Content</th>
-        <td mat-cell *matCellDef="let batch">{{ batch.mercuryContent }}</td>
+        <mat-header-cell *matHeaderCellDef>Mercury Content</mat-header-cell>
+        <mat-cell *matCellDef="let batch">{{ batch.mercuryContent }}</mat-cell>
       </ng-container>
 
       <!-- Cadmium Content Column -->
       <ng-container matColumnDef="cadmiumContent">
-        <th mat-header-cell *matHeaderCellDef>Cadmium Content</th>
-        <td mat-cell *matCellDef="let batch">{{ batch.cadmiumContent }}</td>
+        <mat-header-cell *matHeaderCellDef>Cadmium Content</mat-header-cell>
+        <mat-cell *matCellDef="let batch">{{ batch.cadmiumContent }}</mat-cell>
       </ng-container>
 
       <!-- RoHS Compliance Column -->
       <ng-container matColumnDef="isRoHSCompliant">
-        <th mat-header-cell *matHeaderCellDef>RoHS Compliant</th>
-        <td mat-cell *matCellDef="let batch">{{ batch.isRoHSCompliant }}</td>
+        <mat-header-cell *matHeaderCellDef>RoHS Compliant</mat-header-cell>
+        <mat-cell *matCellDef="let batch">{{ batch.isRoHSCompliant }}</mat-cell>
       </ng-container>
 
       <!-- Quantity Column -->
       <ng-container matColumnDef="quantity">
-        <th mat-header-cell *matHeaderCellDef>Quantity</th>
-        <td mat-cell *matCellDef="let batch">
+        <mat-header-cell *matHeaderCellDef>Quantity</mat-header-cell>
+        <mat-cell *matCellDef="let batch">
           {{ batch.quantity }} {{ batch.unit }}
-        </td>
+        </mat-cell>
       </ng-container>
 
       <!-- Status Column -->
       <ng-container matColumnDef="status">
-        <th mat-header-cell *matHeaderCellDef>Status</th>
-        <td mat-cell *matCellDef="let batch">{{ batch.status }}</td>
+        <mat-header-cell *matHeaderCellDef>Status</mat-header-cell>
+        <mat-cell *matCellDef="let batch">{{ batch.status }}</mat-cell>
       </ng-container>
 
       <!-- Owner Column -->
       <ng-container matColumnDef="owner">
-        <th mat-header-cell *matHeaderCellDef>Owner VAT</th>
-        <td mat-cell *matCellDef="let batch">{{ batch?.company?.VAT }}</td>
+        <mat-header-cell *matHeaderCellDef>Owner VAT</mat-header-cell>
+        <mat-cell *matCellDef="let batch">{{ batch?.company?.VAT }}</mat-cell>
       </ng-container>
 
-  <!-- TODO: combine all 3 buttons into 1 column -->
+      <!-- TODO: combine all 3 buttons into 1 column -->
 
-      <!-- Accept Column -->
+      <!-- Action Column -->
       <ng-container
         $ngIf="batch.status !== 'declined'"
-        matColumnDef="accept-button"
+        matColumnDef="action-button"
+        class="flex-2"
       >
-        <th mat-header-cell *matHeaderCellDef>Accept</th>
-        <td mat-cell *matCellDef="let batch">
-          <button
-            mat-stroked-button
-            color="secondary"
-            *ngIf="batch.status !== 'declined'"
-            (click)="acceptBatch(batch)"
-            routerLinkActive="bg-primary-100"
-            >Accept</button
-          >
-        </td>
-      </ng-container>
-
-      <!-- Reject Column -->
-      <ng-container matColumnDef="reject-button">
-        <th mat-header-cell *matHeaderCellDef>Reject</th>
-        <td mat-cell *matCellDef="let batch">
+        <mat-header-cell class="flex-2" *matHeaderCellDef
+          >Accept</mat-header-cell
+        >
+        <mat-cell class="flex-2" *matCellDef="let batch">
           <button
             *ngIf="batch.status !== 'declined'"
             mat-stroked-button
@@ -100,14 +88,15 @@ import { MatButtonModule } from '@angular/material/button';
           >
             Reject
           </button>
-        </td>
-      </ng-container>
-
-      <!-- Reclaim Column -->
-      <ng-container matColumnDef="reclaim-button">
-        <th mat-header-cell *matHeaderCellDef>Reclaim</th>
-        <td mat-cell *matCellDef="let batch">
-          <!-- TODO: use Status enum -->
+          <button
+            mat-stroked-button
+            color="secondary"
+            *ngIf="batch.status !== 'declined'"
+            (click)="acceptBatch(batch)"
+            routerLinkActive="bg-primary-100"
+          >
+            Accept
+          </button>
           <a
             class="text-inherit"
             *ngIf="batch.status === 'declined'"
@@ -117,13 +106,21 @@ import { MatButtonModule } from '@angular/material/button';
             routerLinkActive="bg-primary-100"
             >Reclaim</a
           >
-        </td>
+        </mat-cell>
       </ng-container>
       <!-- TODO: handle rejected batches -->
 
-      <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-      <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
-    </table>
+      <tr
+        mat-header-row
+        class="flex flex-row items-left"
+        *matHeaderRowDef="displayedColumns"
+      ></tr>
+      <tr
+        mat-row
+        class="flex flex-row items-center"
+        *matRowDef="let row; columns: displayedColumns"
+      ></tr>
+    </mat-table>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -139,9 +136,7 @@ export class InboxTableComponent {
     'quantity',
     'owner',
     'status',
-    'reject-button',
-    'accept-button',
-    'reclaim-button',
+    'action-button',
   ];
 
   async acceptBatch(batch: BatchDto) {
